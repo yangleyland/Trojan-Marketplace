4dde75a41bd022cce6a15c779abeba3e2b4cd06d
Leyland Yang <yangleyland@gmail.com>
2023-04-22 20:06:56 -0700

    add item implemented

--- src/CreateSale.js
+++ src/CreateSale.js
@@ -1,24 +1,69 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import axios from "axios";
 import styled from "styled-components";
 import "./main.css";
 import "./create-sale.css";
+import { useNavigate } from "react-router-dom";
 
 const StyledCreateSale = styled.div`
-    margin-top: 30px;
+  margin-top: 30px;
 `;
 
 function CreateSale() {
+  const [title, setTitle] = useState("");
+  const [description, setDescription] = useState("");
+  const [image, setImage] = useState("");
+  const [price, setPrice] = useState("");
+  const [error, setError] = useState("");
+  const navigate = useNavigate();
+
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+
+    try {
+      const response = await axios.post(
+        "http://localhost:8080/Final_Project/AddItemServlet",
+        `item_name=${encodeURIComponent(
+          title
+        )}&item_description=${encodeURIComponent(
+          description
+        )}&image_url=${encodeURIComponent(image)}&price=${encodeURIComponent(
+          price
+        )}`,
+        {
+          headers: {
+            "Content-Type": "application/x-www-form-urlencoded",
+          },
+        }
+      );
+      navigate("/feed");
+      console.log(response.data);
+    } catch (error) {
+      console.error("Error during login request:", error);
+      setError("An error occurred. Please try again.");
+    }
+  };
   return (
     <StyledCreateSale>
       <div className="v1_2">
-
         <span className="marketplace">Add Item</span>
-        <form>
+        <form onSubmit={handleSubmit}>
           <label htmlFor="title">Title</label>
-          <input type="text" id="title" name="title" />
+          <input
+            onChange={(e) => setTitle(e.target.value)}
+            value={title}
+            type="text"
+            id="title"
+            name="title"
+          />
 
           <label htmlFor="description">Description</label>
-          <textarea id="description" name="description"></textarea>
+          <textarea
+            onChange={(e) => setDescription(e.target.value)}
+            value={description}
+            id="description"
+            name="description"
+          ></textarea>
 
           <label htmlFor="quantity">Quantity</label>
           <input type="text" id="quantity" name="quantity" />
@@ -26,11 +71,23 @@ function CreateSale() {
           <label htmlFor="location">Location</label>
           <input type="text" id="location" name="location" />
 
-          <label htmlFor="pickup-time">Pickup Time</label>
-          <input type="text" id="pickup-time" name="pickup-time" />
+          <label htmlFor="price">Price</label>
+          <input
+            onChange={(e) => setPrice(e.target.value)}
+            value={price}
+            type="text"
+            id="price"
+            name="price"
+          />
 
           <label htmlFor="image">Image</label>
-          <input type="file" id="image" name="image" />
+          <input
+            onChange={(e) => setImage(e.target.value)}
+            value={image}
+            type="text"
+            id="image"
+            name="image"
+          />
 
           <button type="submit">Create Sale</button>
         </form>
--- src/Feed.js
+++ src/Feed.js
@@ -1,87 +1,44 @@
-import React, { useState, useEffect } from 'react';
-import axios from 'axios';
-import './Feed.css';
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import "./Feed.css";
 
 function Feed() {
-
-
   const [items, setItems] = useState([]);
 
   useEffect(() => {
     const fetchData = async () => {
       try {
-        const response = await axios.get('http://localhost:8080/Final_Project/ItemFetchServlet');
+        const response = await axios.get(
+          "http://localhost:8080/Final_Project/ItemFetchServlet"
+        );
         setItems(response.data);
       } catch (error) {
-        console.error('Error fetching data:', error);
+        console.error("Error fetching data:", error);
       }
     };
 
     fetchData();
   }, []);
 
-
   return (
     <div className="rectangles-wrapper">
       <div className="rectangles-container">
-      {items.map(item => (
-        <div className="rectangle">
-          <div className="rectangle-content">
-            <div className="rectangle-img">
-              <img src={item.image_url} alt="Product" />
+        {items.map((item) => (
+          <div className="rectangle">
+            <div className="rectangle-content">
+              <div className="rectangle-img">
+                <img src={item.image_url} alt="Product" />
+              </div>
+              <div className="rectangle-description">
+                <h3>{item.item_name}</h3>
+                <p>{item.item_description}</p>
+              </div>
             </div>
-            <div className="rectangle-description">
-              <h3>{item.item_name}</h3>
-              <p>{item.item_description}</p>
+            <div className="buy-now-btn">
+              <button>Buy Now</button>
             </div>
           </div>
-          <div className="buy-now-btn">
-            <button>Buy Now</button>
-          </div>
-        </div>
         ))}
-        <div className="rectangle">
-          <div className="rectangle-content">
-            <div className="rectangle-img">
-              <img src="https://via.placeholder.com/150" alt="Product" />
-            </div>
-            <div className="rectangle-description">
-              <h3>Lorenzo 1x1 bedroom</h3>
-              <p>I am subletting my bedroom from March to May at 2000 a month</p>
-            </div>
-          </div>
-          <div className="buy-now-btn">
-            <button>Buy Now</button>
-          </div>
-        </div>
-        <div className="rectangle">
-          <div className="rectangle-content">
-            <div className="rectangle-img">
-              <img src="https://via.placeholder.com/150" alt="Product" />
-            </div>
-            <div className="rectangle-description">
-              <h3>USC Backpack</h3>
-              <p>This USC backpack was given to me by Caleb Williams.$50       </p>
-            </div>
-          </div>
-          <div className="buy-now-btn">
-            <button>Buy Now</button>
-          </div>
-        </div>
-        <div className="rectangle">
-          <div className="rectangle-content">
-            <div className="rectangle-img">
-              <img src="https://via.placeholder.com/150" alt="Product" />
-            </div>
-            <div className="rectangle-description">
-              <h3>Carol Folt Mug</h3>
-              <p>I have a mug with Carol Folt on it. Selling for $20.      </p>
-            </div>
-          </div>
-          <div className="buy-now-btn">
-            <button>Buy Now</button>
-          </div>
-        </div>
       </div>
     </div>
   );
